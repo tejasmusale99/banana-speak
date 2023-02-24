6fa00e34a6c634b81c2273baf12e581b2744eddf
Tejas Musale <tejasmusale99@gmail.com>
2023-02-24 11:22:59 +0530

    input output processing Done

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // Reference
 let btnTranslate=document.querySelector("#btn-translate");
 let inputText=document.querySelector("#input-text");
+let outputText=document.querySelector("#output-text")
+
 // console.log(btnTranslate);
 function clickHandler(){
     // console.log("Clicked")
-    console.log("input",inputText.value)
+    // console.log("input",inputText.value)
+    outputText.innerText=inputText.value
 }
 let txtInput=btnTranslate.addEventListener("click",clickHandler);
+ 
